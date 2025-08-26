af867c5e6d97877ed04bfb9859b759f506529176
tuanpham936 <tuan.phamminh936@gmail.com>
2025-08-26 16:26:39 +0700

    Init commit

---
+++ src/main/java/com/communityblogsserver/prj_beginner_communityblogs_backend/PrjBeginnerCommunityblogsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.communityblogsserver.prj_beginner_communityblogs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjBeginnerCommunityblogsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjBeginnerCommunityblogsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/communityblogsserver/prj_beginner_communityblogs_backend/PrjBeginnerCommunityblogsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communityblogsserver.prj_beginner_communityblogs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjBeginnerCommunityblogsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
